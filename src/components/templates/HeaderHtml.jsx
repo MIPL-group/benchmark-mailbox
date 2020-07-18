df7cf5e3aaf1e43e3ae3c3707aa738c525736353
Larry <yuetao1998@gmail.com>
2020-07-18 18:08:33 -0400

    a mailbox web app

---
+++ src/components/templates/HeaderHtml.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const HeaderHtml = () => {
+  return (
+    <nav class="navbar navbar-expand-md navbar-light bg-light fixed-top">
+        <a class="navbar-brand" href="./index.html">Mailbox</a>
+
+        
+
+
+
+    </nav>
+  );
+};
+
+export default HeaderHtml;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import bootstrap from "bootstrap"; // eslint-disable-line no-unused-vars
+import Inbox from "./components/Inbox";
+import HeaderHtml from "./components/templates/HeaderHtml";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function App() {
+  return (
+    <div>
+      <HeaderHtml />
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <div className="container">
+        <Inbox />
+      </div>
+    </div>
+  );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
