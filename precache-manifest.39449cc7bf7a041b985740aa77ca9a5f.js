14588e89685db31f730789b8bda293c72ae6a0c3
Larry <yuetao1998@gmail.com>
2020-07-18 17:59:47 -0400

    Updates

---
+++ precache-manifest.39449cc7bf7a041b985740aa77ca9a5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61cf6ef3279d2963352700852d9f4fea",
+    "url": "/benchmark-mailbox/index.html"
+  },
+  {
+    "revision": "0a9427e99ca9a375a94f",
+    "url": "/benchmark-mailbox/static/js/2.c2b6aeb4.chunk.js"
+  },
+  {
+    "revision": "cf6183876efd003d1702afdecd9682b0",
+    "url": "/benchmark-mailbox/static/js/2.c2b6aeb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "852a1b074c03b8f2ce3a",
+    "url": "/benchmark-mailbox/static/js/main.636b2e93.chunk.js"
+  },
+  {
+    "revision": "306bdadbf92e0f390506",
+    "url": "/benchmark-mailbox/static/js/runtime-main.f8b5c67d.js"
+  }
+]);
